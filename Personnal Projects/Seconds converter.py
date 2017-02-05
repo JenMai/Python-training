19065220f34475a6a636ba6ad7a0d7e2a51a998a
JenniMai <jm.jennimai@gmail.com>
2017-02-05 11:16:19 +0100

    Loop/comments changes

--- Personnal Projects/Seconds converter.py
+++ Personnal Projects/Seconds converter.py
@@ -1,48 +1,55 @@
 #-----------------------------------------------
-# loop until user write the right input:
+# loop until user doesn't want to play anymore (not a really elegant way to do so, admittedly!):
 #-----------------------------------------------
-while True :													
-	try :
-		input_sec = int(raw_input("Enter the seconds you want to convert: ")) 	# turns string from raw_input into an integer
-	except ValueError :															# when input can be turned into an integer
-		print "Sorry, I don't understand, please enter a positive integer (>0) :(."
-		continue 																# inner while loop starts over
+while True :
+    #-----------------------------------------------
+    # loop until user write a legal input:
+    #-----------------------------------------------
+	while True :													
+		try :
+			input_sec = int(raw_input("Enter the seconds you want to convert: ")) 	# turns string from raw_input into an integer
+		except ValueError :															# when input can be turned into an integer
+			print "Sorry, I don't understand, please enter a positive integer (>0) :(."
+			continue 																# inner while loop starts over
 
-	if input_sec < 0 :															# negative int can't be converted
-		print "Sorry, integer must be positive (>0)."
-		continue 
+		if input_sec < 0 :															# negative int can't be converted
+			print "Sorry, integer must be positive (>0)."
+			continue 
 
-	if input_sec <= 59 :														# no need to convert
-		print str(input_sec) + " second(s), of course!"
-		continue 
+		if input_sec <= 59 :														# no need to convert
+			print str(input_sec) + " second(s)."
+			continue 
+		else :
+			break 																	# when input is an integer > 59, break the loop
+	 
+    #-----------------------------------------------
+    # functions to get time in minutes/seconds/hours :
+    #-----------------------------------------------
+	def get_min(time) :
+		min = (time/60)%60 															# result must stay between 0 and 59.
+		if min >= 0 :
+			return min
+
+	def get_sec(time) :
+		sec = time%60 																# result must stay between 0 and 59.
+		if sec >= 0 :
+			return sec
+
+	def get_hour(time) :
+		hour = time/3600
+		if hour >= 0 :
+			return hour
+			
+	print str(get_hour(input_sec)) + " hour(s), " + str(get_min(input_sec)) + " minute(s) and " + str(get_sec(input_sec)) + " second(s)."
+	
+    #-----------------------------------------------
+    # determine if user wants to play again
+    #-----------------------------------------------
+	yes_no = raw_input("Try again ? (y/n)").lower()
+	if (yes_no == "y"):
+		continue 																	# starts over
+	elif (yes_no == "n"):
+		break 																		# program ends
 	else :
-		break 																	# when input is an integer > 59, break the loop
- 
-#-----------------------------------------------
-# function to get minutes :
-#-----------------------------------------------
-def get_min(time) :
-	min = (time/60)%60 															# result must stay between 0 and 59.
-	if min >= 0 :
-		return min
-
-#-----------------------------------------------
-# function to get seconds :
-#-----------------------------------------------
-def get_sec(time) :
-	sec = time%60 																# result must stay between 0 and 59.
-	if sec >= 0 :
-		return sec
-
-#-----------------------------------------------
-# function to get hours :
-#-----------------------------------------------
-def get_hour(time) :
-	hour = time/3600
-	if hour >= 0 :
-		return hour
-		
-#-----------------------------------------------
-# result is given from the 3 functions :
-#-----------------------------------------------
-print str(get_hour(input_sec)) + " hour(s), " + str(get_min(input_sec)) + " minute(s) and " + str(get_sec(input_sec)) + " second(s)."
+		print "I don't understand."
+		break
